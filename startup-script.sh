2da003da725422c2581ea383e376cc670659ace9
Rattanjot Singh Tiwana <tiwanarattan71@gmail.com>
2024-12-09 17:10:15 -0500

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -3,4 +3,5 @@ echo "Running startup script" > /var/log/startup-script.log
 sudo apt update
 sudo apt install -y nginx
 curl -o /var/www/html/index.html https://raw.githubusercontent.com/rattanjot-singh-tiwana/Cloud-Build-Powered-Nginx-Infra-with-Terraform/refs/heads/main/index.html
+rm -f /var/www/html/index.nginx-debian.html
 echo "Startup script completed" >> /var/log/startup-script.log
